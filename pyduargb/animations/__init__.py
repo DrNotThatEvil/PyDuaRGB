44603cd817badadcdf8df6b1d7ea862436f59aa5
Willmar Knikker <wil7vin@live.nl>
2017-08-11 23:35:43 +0200

    Added fade in animation

--- pyduargb/animations/__init__.py
+++ pyduargb/animations/__init__.py
@@ -3,15 +3,18 @@ from __future__ import print_function, absolute_import, unicode_literals
 from .pulse import Pulse
 from .jirate import Jirate
 from .static import Static
+from .fadein import Fadein
 
 ALL_ANIMATIONS = [
    Pulse,
    Jirate,
-   Static
+   Static,
+   Fadein
 ]
 
 ANIMATION_MAP = {
     'pulse': Pulse,
     'jirate': Jirate,
-    'static': Static
+    'static': Static,
+    'fadein': Fadein
 }
